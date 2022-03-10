a2fd0179094563572af39296f2280680affc94a7
TheKiesling <kie21581@uvg.edu.gt>
2022-03-09 22:23:38 -0600

    New And Ready

--- RAM.py
+++ RAM.py
@@ -30,6 +30,7 @@
         c) READY: when leaving the CPU and the randomly generated number is 2, then it goes back to the “ready” queue.
 '''
 
+from urllib.request import CacheFTPHandler
 import simpy as simpPI; #SIMP de PI 
 import random as rnd;
 
@@ -39,19 +40,38 @@ AMOUNT_CPU = 1
 CAPACITY = 100
 INTERVAL = 10
 PROCESS = 25
+INSTRUCTIONS = 3
 
 #----------------------------CLASSES---------------------------
-class memory():
-        def __init__(self, env, cpus, ram_capacity):
-            self.cpu = simpPI.Resource(env, cpus)
-            self.ram = simpPI.Container(env, init = ram_capacity, capacity = ram_capacity)
+class RAMMemory():
+    def __init__(self, env):
+        self.cpu = simpPI.Resource(env, capacity = 1)
+        self.ram = simpPI.Container(env, init = 100, capacity = 100)
 
-class process():
-    def __init__(self, id, env):
+class Program():
+    def __init__(self, id, env, RAM_Memory):
         self.env = env
         self.name = id
+        self.action = env.process(self.newProgram(self.name, RAM_Memory))
+    
+    def newProgram(self, id, RAM_Memory):
         self.memory = rnd.randint(1,10)
+        print("-----------------------NEW---------------------------")
+        print(f'NEW: Process {id} at {env.now} with memory: {self.memory}')
+        print("-----------------------------------------------------")
+        print()
+        with RAM_Memory.cpu.request() as req:
+            yield req
+            yield RAM_Memory.ram.get(self.memory)
+            self.readyProgram(id)
+    
+    def readyProgram(self, id):
         self.instructions = rnd.randint(1,10)
+        print("-----------------------READY--------------------------")
+        print(f'READY: Process {id} at {env.now} with {self.instructions} instructions')
+        print("-----------------------------------------------------")
+        print()
+    
     
     def _getname_(self):
             return self.name
@@ -62,3 +82,14 @@ class process():
     def _getinstructions_(self):
             return self.instructions      
 
+def newProcess(env, RAM_Memory):
+    for i in range(PROCESS):
+        Program(i, env, RAM_Memory)
+        t = rnd.expovariate(1.0 / INTERVAL)
+        yield env.timeout(t)
+    
+
+env = simpPI.Environment()
+RAM_Memory = RAMMemory(env)
+env.process(newProcess(env, RAM_Memory))
+env.run(100000)
